a6da5b442982a3aa5023f76621092c7d3df90e7c
Valentin Moullet <valentin@tsf-444-wpa-2-233.epfl.ch>
2016-11-19 15:55:21 +0100

    Fix

--- LauzHackApp/models.py
+++ LauzHackApp/models.py
@@ -4,17 +4,17 @@ from django.db import models
 
 # Create your models here.
 
-class Video(models.Model):
-	url = models.CharField(max_length=200)
-	# In seconds
-	length = models.IntegerField()
-	stats = models.ManyToMany(Stat)
-	nb_views = models.IntegerField(default=0)
-	avg_age = models.DecimalField(max_digits=5, decimal_places=3, default=0.0)
-
 class Stat(models.Model):
 	avg_happiness = models.DecimalField(max_digits=9, decimal_places=8)
 	avg_sadness = models.DecimalField(max_digits=9, decimal_places=8)
 	avg_surprise = models.DecimalField(max_digits=9, decimal_places=8)
 	# In seconds
 	timestamp = models.IntegerField()
+
+class Video(models.Model):
+	url = models.CharField(max_length=200)
+	# In seconds
+	length = models.IntegerField()
+	stats = models.ManyToManyField(Stat)
+	nb_views = models.IntegerField(default=0)
+	avg_age = models.DecimalField(max_digits=5, decimal_places=3, default=0.0)
